803a584ccfcf713366713671b29be74a3fcc8945
nehannin <nelli.hanninen@edu.lapinamk.fi>
2024-04-25 18:54:53 +0300

    Added Dockerfile + Docker-compose.yml

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/")
-def sentiment_analysis():
-    # CODE HERE
-    return "<p>Sentiment Analysis</p>"
+def sentiment_analysis_endpoint():
+    return "Sentiment Analysis"
+
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=8080)
